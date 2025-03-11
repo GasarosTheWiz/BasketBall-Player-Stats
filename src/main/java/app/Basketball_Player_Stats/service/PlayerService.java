73dac463a4920beded85cd5524f534b829da9e73
Dimitris Drakos <drakos.tem@gmail.com>
2025-03-11 18:12:22 +0200

    Update PlayerService.java

--- src/main/java/app/Basketball_Player_Stats/service/PlayerService.java
+++ src/main/java/app/Basketball_Player_Stats/service/PlayerService.java
@@ -16,22 +16,22 @@ public class PlayerService {
     @Autowired
     private PlayerRepository playerRepository;
 
-    // Μέθοδος για τη διαγραφή όλων των παικτών με το ίδιο όνομα
+    
     public void deleteByName(String name) {
-        List<Player> players = playerRepository.findByName(name); // Βρίσκουμε όλους τους παίκτες με αυτό το όνομα
+        List<Player> players = playerRepository.findByName(name); 
         if (!players.isEmpty()) {
-            playerRepository.deleteAll(players); // Διαγράφουμε όλους τους παίκτες με το συγκεκριμένο όνομα
+            playerRepository.deleteAll(players); 
         } else {
             throw new RuntimeException("Player with name " + name + " not found");
         }
     }
 
-    // Μέθοδος για την αποθήκευση ενός παίκτη
+    
     public Player savePlayer(Player player) {
         return playerRepository.save(player);
     }
 
-    // Μέθοδος για την ανάκτηση όλων των παικτών
+    
     public List<Player> getAllPlayers() {
         return playerRepository.findAll();
     }
